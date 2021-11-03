3c5f15eb88acf1599b1d0faa4d488eda30816c0d
Jfisher37 <joseph.eric.fisher@gmail.com>
2021-11-02 21:26:45 -0400

    added get routes.

--- controllers/api/workout-routes.js
+++ controllers/api/workout-routes.js
@@ -8,13 +8,40 @@ const mongojs = require("mongojs");
 const db = mongojs(databaseUrl, collections);
 
 router.get("/", (req, res) => {
-    db.workouts.find({}, (error, data) => {
+
+    db.workouts.aggregate([
+      {
+        $addFields: {
+          totalDuration: { $sum: "$exercises.duration" },
+        },
+      },
+    ]).sort({ day: -1}, (error, data) => {
       if (error) {
         res.send(error);
       } else {
+
         res.json(data);
       }
     });
   });
 
+  router.get("/range", (req, res) => {
+
+    db.workouts.aggregate([
+      {
+        $addFields: {
+          totalDuration: { $sum: "$exercises.duration" },
+        },
+      },
+    ]).sort({ day: -1}, (error, data) => {
+      if (error) {
+        res.send(error);
+      } else {
+
+        res.json(data);
+      }
+    });
+  });
+
+  
   module.exports = router;
