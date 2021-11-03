e06c13d411714744c5a7160e9007eed3da3cd129
Jfisher37 <joseph.eric.fisher@gmail.com>
2021-11-02 20:28:15 -0400

    added HTML routes.

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
+const statsRoutes = require('./stats-routes');
+const exerciseRoutes = require('./exercise-routes');
 
+router.use('/stats', statsRoutes);
+router.use('/exercise', exerciseRoutes);
 router.use('/api', apiRoutes);
 
   module.exports = router;
